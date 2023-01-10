7f5a1ad6a8d48c38fd6285c72e1a1b3c142dce00
machkouroke <machkour20.mo1@gmail.com>
2023-01-11 00:18:51 +0100

    `🚀 perf` Move of data loading and join in a separate class

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,39 +1,59 @@
 package org.lop
 
 
-import constant.FileType
-import utilities.Loader
-import utilities.Transformer.Transformer
+import utilities.{Loader, Prono}
 
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.{DataFrame, SparkSession}
-
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.util.SizeEstimator
+import scala.util.Properties
 
 object Main {
 
     def main(args: Array[String]): Unit = {
-        val conf = new SparkConf().setAppName("LOPPronostic").set("spark.logging.level", "INFO")
-        val spark: SparkSession = SparkSession.builder().config(conf).getOrCreate()
+        val conf = new SparkConf().setAppName("LOPPronostic")
+          .set("spark.logging.level", "INFO")
+          .set("spark.broadcast.blockSize", "90m")
+
+
+        val spark: SparkSession = SparkSession.builder().config(conf)
+          .getOrCreate()
+        val numExecutors = spark.sparkContext.defaultParallelism
+        val scalaVersion = Properties.versionString
+        val sparkVersion = spark.version
+
         val fs: FileSystem = FileSystem.get(spark.sparkContext.hadoopConfiguration)
+        val path = "/data/sports-data/all_data.parquet"
+        if (!fs.exists(new Path(path))) {
+            println("Not found")
+            Loader.saveData(path, spark, fs)
+        }
+        println("Data saved")
+        val matchs = spark.read.parquet(path)
+        val pronoVersion = "Pronostiqueur 5.0"
+        println("Jar version:4.0")
+        println(s"Spark version: $sparkVersion")
+        println(s"Scala version: $scalaVersion")
+        println(s"Nombre d'exécuteurs par défaut : $numExecutors")
+        println(s"Estimated size of the dataFrame match = ${SizeEstimator.estimate(matchs) / 1000000} mb")
+        println("Data loaded")
+        /* Prédiction */
+                println(Prono.makeProno(
+                    "Aston Villa",
+                    "Wigan",
+                    matchs,
+                    pronoVersion,
+                    spark
+                ))
+        println("Fin du programme")
 
-        /* Chargement des données */
-        var matchs: DataFrame = Loader
-          .listDatasets(
-              spark,
-              fs,
-              Loader.listFiles(fs.listFiles(new Path("/data/sports-data"), true))
-          ).join()
-
-        /* Transformation des données */
-        matchs = new Transformer(matchs)
-          .typeTransform(FileType.colsInteger, FileType.colsFloat)
-          .renameColumns()
-          .dropNa()
-          .data
-        matchs.printSchema()
-        matchs.show(2)
-//        PronoModel.fit(matchs, "FTRindexed")
+        println(matchs.count())
+//        Prono.fitModel(
+//            pronoVersion,
+//            matchs,
+//            spark
+//        )
     }
 }
 
--- src/main/scala/utilities/Loader.scala
+++ src/main/scala/utilities/Loader.scala
@@ -3,11 +3,37 @@ package utilities
 
 import org.apache.hadoop.fs.{FileSystem, LocatedFileStatus, Path, RemoteIterator}
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import constant.FileType
+import utilities.Transformer.Transformer
 
 import java.net.URI
 import scala.annotation.tailrec
 
 object Loader {
+    def saveData(path: String, spark: SparkSession, fs: FileSystem): Unit = {
+
+
+        /* Chargement des données */
+        val matchs: DataFrame = Loader
+          .listDatasets(
+              spark,
+              fs,
+              Loader.listFiles(fs.listFiles(new Path("/data/sports-data"), true))
+          ).join()
+
+        /* Transformation des données */
+        val data = new Transformer(matchs)
+          .renameColumns()
+          .typeTransform(
+              FileType.colsInteger,
+              FileType.colsFloat
+          )
+          .dropNa()
+          .data.
+          write
+          .parquet(path)
+    }
+
     def listFiles(iter: RemoteIterator[LocatedFileStatus]): Seq[URI] = {
         @tailrec
         def go(iter: RemoteIterator[LocatedFileStatus], acc: List[URI]): List[URI] = {
