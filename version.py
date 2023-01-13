e0372b13283886673bf0040e1e4dd0cbcfb17206
machkouroke <machkour20.mo1@gmail.com>
2023-01-13 14:04:28 +0100

    `fix` Add of selection of main part of version

--- version.py
+++ version.py
@@ -3,5 +3,5 @@ with open("build.sbt") as f:
     # print(f.readlines())
     lines: list[str] = [x for x in f.readlines() if x.startswith("ThisBuild")]
     versionJar: str = lines[0].split(":=")[1].strip().strip('"')
-    versionScala: str = lines[1].split(":=")[1].strip().strip('"')
+    versionScala: str = ".".join(lines[1].split(":=")[1].strip().strip('"').split(".")[:2])
     print(versionJar, versionScala, sep="\n")
